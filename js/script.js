e09498591c3c9b9eb3cf568420ade15253dbcd25
Nurnoby Islam <44687099+Nurnoby@users.noreply.github.com>
2019-02-10 15:46:54 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();   
+});
+
+
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();   
+});
+
+$(document).ready(function(){
+  $('.toast').toast('show');
+});
